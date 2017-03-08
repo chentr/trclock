4ff4a5dcfef0a56dabd967f2f30784c05fc88748
chentr <peter__barnes@hotmail.com>
2017-03-08 20:44:23 +0800

    cmd handle

--- main.c
+++ main.c
@@ -3,21 +3,6 @@
 	2.write GUI for it.
 	clock about specific time, clock from current time
 */
-
-// get user's input 
-	//check if set  clock ,or modify clock , delete clock
-
-// check all clocks's next point
-	//if need alarm clock 
-		//show alert betweent -5 to 5 second
-		//3{
-			//if it is one time clock
-				//disable it
-			//else if it is a repeat clock
-				//set alarm to next point
-		//3}
-	//if a clock is out of time
-		//3
 #include <stdio.h>		
 #include <stdlib.h>		
 #include <string.h>		
@@ -27,11 +12,12 @@
 #define MAX_TIME_STRLEN 64
 #define MAX_CLOCK_NUM 10
 #define DAY_PER_WEEK 7
+#define MAX_PERLINE 128
 struct Week_alarm{
 	bool wday[DAY_PER_WEEK];//set 1 if this day want to be alarmed
 	int hour;
 	int min;
-	int second;
+	int sec;
 };
 enum ALARM_TYPE{
 	AL_DISABLE,
@@ -45,18 +31,6 @@ struct Alarm{
 	time_t repeat_time;//repeat per repeat_time. time_t == long int,means seconds.
 	struct Week_alarm week_time;//alarm in a time of a week's specific days.
 } alarm[MAX_CLOCK_NUM];
-/*
-void show_time()
-{
-	time_t now = time (NULL);;   
-	char time_str[MAX_TIME_STRLEN];
-	struct tm *local=localtime(&now);
-	//difftime
-	printf("current time is :%s\n",ctime(&now));
-	printf("Local hour is: %d\n",local->tm_hour);
-	return;
-}
-*/
 
 /*get current week day*/
 int get_wday()
@@ -66,22 +40,102 @@ int get_wday()
 	return local->tm_wday;/*0-6*/
 }
 
-int main()
+void print_alarm()
 {
-	return 0;
+    printf("--------------------------current alarms---------------------------\n");
+    int i,j;
+    struct tm *p;
+    for(i=0;i<MAX_CLOCK_NUM;i++)
+    {
+        if(AL_ONETIME == alarm[i].type)
+        {
+            printf("id=%d,type:onetime alarm \n",i);
+            printf("next alarm time:\t%s\n",ctime(&alarm[i].next_point));
+            printf("\n");
+        }
+        else if(AL_REPEAT == alarm[i].type)
+        {
+            printf("id=%d,type:repeat per duration\n",i);
+            printf("next alarm time:\t%s\n",ctime(&alarm[i].next_point));
+            p=gmtime(&alarm[i].next_point);
+            printf("repeat period:\t%d day;%d:%d:%d\n",p->tm_mday,p->tm_hour, p->tm_min, p->tm_sec);
+            printf("\n");
+        }
+        else if(AL_WEEK == alarm[i].type)
+        {
+            printf("id=%d,type:in time of weekday\n",i);
+            printf("next alarm time:\t%s\n",ctime(&alarm[i].next_point));
+            printf("in these day of a week:\t");
+            for(j=0;j<DAY_PER_WEEK;j++)
+            {
+                if(true == alarm[i].week_time.wday[j])
+                {
+                    printf("%d\t",j+1);
+                }
+            }
+            printf("\n");
+            printf("alarm time:%d:%d:%d\t",alarm[i].week_time.hour,alarm[i].week_time.min,alarm[i].week_time.sec);
+            printf("\n");
+        }
+    }
+    printf("--------------------------current alarms end-----------------------\n");
+
 }
 
-//
-//<clock_id> <cmd> <time>
-//<clock_id> = 1-10
-//cmd: 0 disable , 1 one time, 2 repeat per time, 3 time by week.
-// cmd=0(disable): <time> not need
-// cmd=1(one time): <time> "yyyymmddhhmmss".
-// cmd=2(repeat per time): <time> "yyyymmddhhmmss dhhmmss",at most 9 days.
-// cmd=3(time by week): <time> "hhmmss [1234567]".
-int handle_cmd()
+void print_help()
 {
+    printf("--------------------------command list---------------------------\n");
+    printf("<clock_id> <type> <time>\n");
+    printf("<clock_id> = 0-9\n");
+    printf("type: 0 disable , 1 one time, 2 repeat per time, 3 time by week.\n");
+    printf("type=0(disable): <time> not need\n");
+    printf("type=1(one time): <time> = <yyyy,mm,dd,hh,mm,ss>.\n");
+    printf("type=2(repeat per time): <time> <yyyy,mm,dd,hh,mm,ss,d,hh,mm,ss>,at most 9 days.\n");
+    printf("type=3(time by week): <time> <hhmmss,[1234567]>.\n");
+    printf("--------------------------command list end-----------------------\n");
+    printf("[user input]:");
+    return ;
+}
 
+void handle_cmd()
+{
+    char cmd[MAX_PERLINE];
+    int clock_id;
+    int type;
+    char time_cmd[MAX_PERLINE];
+    struct tm time;
+    fgets(cmd,MAX_PERLINE-1,stdin);
+    scanf("%d %d %s\n", &clock_id, &type, time_cmd);
+    if(clock_id<0 || clock_id>9 || type < AL_DISABLE ||type > AL_WEEK)
+    {
+        printf("cmd error,wrong clock_id or type\n");
+        return ;
+    }
+    alarm[clock_id].type = type;
+    if(AL_ONETIME == type)
+    {
+        sscanf(time_cmd,"%d,%d,%d,%d,%d,%d",&(time.tm_year),&(time.tm_mon),&(time.tm_mday),&(time.tm_hour),&(time.tm_min),&(time.tm_sec));
+    }
+	else if(AL_REPEAT == type)
+    {
+    
+    }
+	else if(AL_WEEK == type)
+    {
+
+    }
+	
+
+}
+
+int user_inter()
+{
+    while(1)
+    {
+        print_alarm();
+        print_help();
+        handle_cmd();
+    }
 }
 
 //set to next alarm point or disable it*/
@@ -125,11 +179,6 @@ int reset_clock(struct Alarm alarm)
 	return 0;
 }
 
-void set_next(struct Alarm alarm)
-{
-
-}
-
 void check_thread()
 {
 	// check all clocks's next point
@@ -157,4 +206,10 @@ void check_thread()
 		}
 	}
 	apr_sleep(1000000);
+}
+
+int main()
+{
+    user_inter();
+	return 0;
 }
